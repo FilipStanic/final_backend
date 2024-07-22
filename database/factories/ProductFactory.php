6b5d90a8c00453e16a54e185ad99c187582245ee
Filip <filipkostanic@gmail.com>
2024-07-22 19:43:54 +0200

    First Commit

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Product;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->word,
+            'price' =>  0.00,
+            'description' => '',
+            'image_path' => '',
+            'category' => 'test'
+        ];
+    }
+}
